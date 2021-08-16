822c7791cefd15c7104964c9ef4435aa5799b019
narayanvyas <narayanvyas87@gmail.com>
2021-08-16 09:14:58 +0530

    Added PPT For Workshop

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_workshop/home.dart';
+import 'package:flutter_workshop/ui/home.dart';
 import 'package:get/get.dart';
 
 void main() async {
